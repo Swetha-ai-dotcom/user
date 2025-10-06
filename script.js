f1f498fab46cbb885037a64baa8659011c3d0b25
Swetha R <swetha3933@gmail.com>
2025-10-06 10:36:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-fetch("blogs.json")
-  .then(res => res.json())
-  .then(data => {
-    const container = document.getElementById("blog-container");
-    data.forEach(blog => {
-      const div = document.createElement("div");
-      div.innerHTML = `<h2>${blog.title}</h2><p>${blog.content}</p><hr>`;
-      container.appendChild(div);
-    });
-  });
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDfIloonJFKz7X9aGtiZkosrnXVIvUWWA8",
+    authDomain: "customerblogs.firebaseapp.com",
+    projectId: "customerblogs",
+    storageBucket: "customerblogs.firebasestorage.app",
+    messagingSenderId: "470087800879",
+    appId: "1:470087800879:web:6ac3e086cd6a37320c0eda"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
